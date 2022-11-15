93d0bbe765dc34bafd828d3d47721df39193b50c
David Ryder <67267592+david-ryder@users.noreply.github.com>
2022-11-15 13:29:30 -0500

    implementing login system

---
+++ djangoproject/login/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def profile(request):
+    return render(request, 'profile.html')
